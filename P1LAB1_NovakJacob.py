d8b76f191cfa544c9acc7eba8c620423e504e3d8
Jnovak29 <Jakenovak29@gmail.com>
2025-09-21 22:44:57 -0400

    Add files via upload

---
+++ P1LAB1_NovakJacob.py
@@ -0,0 +1,9 @@
+#Jacob Novak
+#9/21/2025
+#P1LAB1
+#Lab for CTI-110
+
+#Enter first name.
+first_name = (input("Enter your first name:"))
+last_name = (input("Enter last name:"))
+print ("Hello, welcome to CTI-110!", first_name, last_name)
